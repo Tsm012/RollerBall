ea49455d2057495f07a98107bc170bb76b1491d2
Shantzformation <Tsm012@bucknell.edu>
2025-05-02 22:21:48 -0700

    scoreboard

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,25 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb; 
     private float movementX;
     private float movementY;
+    private int count;
     public float speed = 0; 
+    public TextMeshProUGUI countText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent <Rigidbody>(); 
+        SetCountText();
+    }
+    
+    void SetCountText() 
+    {
+        countText.text =  "Count: " + count.ToString();
     }
 
     // Update is called once per frame
@@ -25,6 +34,21 @@ public class PlayerController : MonoBehaviour
         movementX = movementVector.x; 
         movementY = movementVector.y; 
     }
+    
+    
+    
+    void OnTriggerEnter(Collider other) 
+    {
+        if (other.gameObject.CompareTag("PickUp")) 
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+    
+  
+    
     private void FixedUpdate() 
     {
         Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
